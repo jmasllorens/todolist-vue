a1e5fcd03a6b55a51554c39e8dbf830718d1f968
Jael Masllorens <jmasllorens@gmail.com>
2021-07-22 01:04:00 +0200

    Retoque de imágenes dinámicas

--- src/views/MovieDetails.vue
+++ src/views/MovieDetails.vue
@@ -1,8 +1,10 @@
 <template>
 <div>
   <h2 style="margin-bottom: 2.5rem;"><em>{{ movie.spanishTitle }}</em></h2>
+  <article style="height: auto; maegin-top: 3.5rem; margin-bottom: 4rem;">
     <!-- <img v-bind:src="`../assets/:${movie.img}.jpg`" style="width: 10%; margin-bottom: 1.5rem;"> -->
-  <img :src="require(`@/assets/${movie.img}.jpg`)" style="width: 20%; margin-bottom: 1.5rem;" alt class="icon" />
+    <img :src="require(`@/assets/${movie.img}.jpg`)" style="width: 20%; border-radius: 1rem;"/>
+  </article>
   <h4>ID number: {{ $route.params.id }}</h4>
   <h3>Original title: "{{ movie.title}}"</h3>
   <h3>Director: {{ movie.director }}</h3>
