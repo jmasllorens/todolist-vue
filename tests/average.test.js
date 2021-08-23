608ab20820cf9807289dc93ec4cc5cfdf3c23dc7
Jael Masllorens <jmasllorens@gmail.com>
2021-08-23 09:31:53 +0200

    Se añaden los tests para la función average

---
+++ tests/average.test.js
@@ -0,0 +1,16 @@
+const { average } = require('../utils/for_testing')
+
+describe ('average', () => {
+
+    test('of one value is the value itself', () => {
+        expect(average([1])).toBe(1)
+    })
+
+    test('of empty array is zero', () => {
+        expect(average([])).toBe(0)
+    })
+
+    test('of undefined array is undefined', () => {
+        expect(average(undefined)).toBeUndefined()
+    })
+})
--- utils/for_testing.js
+++ utils/for_testing.js
@@ -8,6 +8,10 @@ const palindrome = (string) => {
 }
 
 const average = array => {
+    if (array == undefined) return undefined
+
+    if (array.length === 0) return 0
+
     let sum = 0
     array.forEach(num => { sum += num })
     return sum / array.length
